a1fd90ec79bd17234dc08699ea075917f01bbfdc
Anastasia <asbryansk@yandex.ru>
2023-07-10 23:27:49 +0300

    1

---
+++ 1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Input your first number");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input your second number");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+ if (firstNum > secondNum) 
+ Console.WriteLine(+firstNum+ "  is the biggest number and the " +secondNum+ " is the smallest");
+  else if (firstNum == secondNum) Console.WriteLine("Your numbers are equal");
+ else  Console.WriteLine(+secondNum+ " is the biggest number and the " +firstNum+ " is the smallest");
