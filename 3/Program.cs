b16b41713262c5bce4ea24f5e871e53caddcc945
Anastasia <asbryansk@yandex.ru>
2023-07-10 23:28:43 +0300

    3

---
+++ 3/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Input your number");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number%2 == 0) 
+Console.WriteLine("Your number "+number+ " is even");
+ else Console.WriteLine("Your number is odd");
