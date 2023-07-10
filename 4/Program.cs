5c3fc1eed5e041971e5192b023ea7327ba323ce5
Anastasia <asbryansk@yandex.ru>
2023-07-10 23:29:09 +0300

    4

---
+++ 4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Input your number");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+while (number <= N)
+{
+  if (number%2 == 0)
+  {
+  Console.Write(number +",");
+  }
+number = number + 1;
+}
